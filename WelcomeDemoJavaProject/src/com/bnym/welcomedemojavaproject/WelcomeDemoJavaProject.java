d578932da26f02fc9191433e13052d0da61e8d73
Ramesh_xecckh4 <ramesh.thiruvenkataraju@bnymellon.com>
2018-09-14 14:11:49 +0530

    Initial Commit.

---
+++ WelcomeDemoJavaProject/src/com/bnym/welcomedemojavaproject/WelcomeDemoJavaProject.java
@@ -0,0 +1,14 @@
+package com.bnym.welcomedemojavaproject;
+
+public class WelcomeDemoJavaProject {
+
+	public static void main(String[] args) {
+		System.out.println ("The name is-"+getName());
+
+	}
+	
+	private static String getName(){
+		return "Welcome Demo Java Project.";
+	}
+
+}
